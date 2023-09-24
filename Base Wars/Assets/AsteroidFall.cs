2b518d80695295734c5260c7ef9f276e50dae4d9
Dev Gandhi <devgandhi914@gmail.com>
2023-09-23 23:58:52 -0400

    Multiples of the asteroids are instantiated

--- Base Wars/Assets/AsteroidFall.cs
+++ Base Wars/Assets/AsteroidFall.cs
@@ -8,6 +8,9 @@ public class AsteroidFall : MonoBehaviour
     public float moveSpeed = 5;
     public float deadZone = -45;
     public GameObject Ass_Steroid;
+    public float spawnRate = 2;
+    private float timer = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,22 @@ public class AsteroidFall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else {
+            spawnAss_Steroids();
+            timer = 0;
+        }
+
+
         transform.position = transform.position + (Vector3.right * moveSpeed) * Time.deltaTime;
         transform.position = transform.position + (Vector3.down * moveSpeed) * Time.deltaTime;
     }
 
     void spawnAss_Steroids()
     {
-        Instantiate(Ass_S)
+        Instantiate(Ass_Steroid, transform.position, transform.rotation);
     }
 }
